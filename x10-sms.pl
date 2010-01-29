4285957406992ac230b4a420db4cdc2cdfb9e384
Gerry Kavanagh <gerryk@gmail.com>
2010-01-29 22:13:12 +0000

    First working version

--- x10-sms.pl
+++ x10-sms.pl
@@ -8,6 +8,8 @@
 # K: kitchen (underfloor)
 # C: central heating
 #
+# Commands: [ON|OFF]
+#
 # We are expecting the commands in the form X Y, where X is the zone 
 # and Y is the command. This implies that there should always be an even
 # number of elements in the SMS string, unless it is a '?' which queries
@@ -15,30 +17,44 @@
 # So, let's first split out all the elements and count them...
 
 @elements = @ARGV;
+$errormsg = "";
+%devices = ( 	'U' => 'A1',
+		'D' => 'A2',
+		'K' => 'A3',
+		'C' => 'A4'	);
 
 if (@elements % 2)	{
-	# check if '?'
+	if (@elements[0] == '?')	{
+		# do a status SMS
+	} else	{
+		$cmd = "echo \"Not a clue, mate...\" | gnokii --sendsms 0877996336";
+		$result = system($cmd);
+	}
 } else	{
-	$NumberCommands = @elements/2;
 	for($i=0;$i<@elements;$i+=2)	{
 		# validate commands
-		if (index("UDKC",uc @elements[$i]))	{
-			if (index("ONOFF",uc @elements[$i+1]))	{
+		if (index('UDKC',uc @elements[$i])!=-1)	{
+			if (index('ONOFF',uc @elements[$i+1])!=-1)	{
 				$commands{uc @elements[$i]}=uc @elements[$i+1];
 			}	else	{
 				$InvalidCommand = 1;
+				$errormsg .= "'".@elements[$i+1]."'"." incorrect command\n";
 			}
 		}	else	{
 			$InvalidSection = 1;
+			$errormsg .= "'".@elements[$i]."'"." incorrect circuit\n";
 		}
 	}
 	if ($InvalidSection == 0 && $InvalidCommand == 0)	{
 		# process commands
-		for my $key ( keys %commands ) {
-        		my $value = $commands{$key};
-	        	print "$key => $value\n";
+		while (($circuit, $command) = each %commands ) {
+        		$cmd = 'heyu '.$command.' '.$devices{$circuit};
+			$result = system($cmd);
 		}
 		# generate SMS reply
+	} else	{
+		$cmd = "echo \"$errormsg\n" | gnokii --sendsms 0877996336";
+		$result = system($cmd);
 	}
 }
 
